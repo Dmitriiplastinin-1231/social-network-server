1d0a051f041b271af26dbaaa34354ee7a70c6a9e
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2023-06-18 13:05:33 +0500

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const users = async (req, res) => {
+    res.send('users');
+}
+
+module.exports = {
+    users
+}
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+const { users } = require('../controllers/users');
+var router = express.Router();
+
+// /users
+router.get('/', users);
+
+
+module.exports = router;
